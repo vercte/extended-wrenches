c2ce730b5b18dede6a493ed54ce3ab2cd3fe2fd5
vercte <lopstedener@gmail.com>
2025-07-05 22:29:01 +0700

    base create wrench dying is back!!

--- src/main/java/net/vercte/extendedwrenches/ExtendedWrenchesRecipeSerializers.java
+++ src/main/java/net/vercte/extendedwrenches/ExtendedWrenchesRecipeSerializers.java
@@ -2,8 +2,10 @@ package net.vercte.extendedwrenches;
 
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.crafting.RecipeSerializer;
+import net.minecraft.world.item.crafting.SimpleCraftingRecipeSerializer;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import net.vercte.extendedwrenches.wrench.WrenchDyeRecipe;
 import net.vercte.extendedwrenches.wrench.WrenchMaterialSwapRecipe;
 
 import java.util.function.Supplier;
@@ -12,6 +14,7 @@ public class ExtendedWrenchesRecipeSerializers {
     public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(BuiltInRegistries.RECIPE_SERIALIZER, ExtendedWrenches.ID);
 
     public static final Supplier<RecipeSerializer<WrenchMaterialSwapRecipe>> WRENCH_MATERIAL_SWAP = RECIPE_SERIALIZERS.register("wrench_material_swap", WrenchMaterialSwapRecipe.Serializer::new);
+    public static final Supplier<RecipeSerializer<WrenchDyeRecipe>> WRENCH_DYE = RECIPE_SERIALIZERS.register("wrench_dye", () -> new SimpleCraftingRecipeSerializer<>(WrenchDyeRecipe::new));
 
     public static void register(IEventBus modEventBus) {
         RECIPE_SERIALIZERS.register(modEventBus);
